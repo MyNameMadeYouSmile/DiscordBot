bc033acfe0c53e026f475ad15bd2b377526b12b7
MyNameMadeYouSmile <58444275+MyNameMadeYouSmile@users.noreply.github.com>
2019-12-04 14:40:15 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -101,7 +101,38 @@ async def newgwa(ctx):
     
 @client.command(pass_context=True)
 async def search(ctx, *, query):
-  
+  ua = UserAgent()
+
+  google_url = "https://www.google.com/search?q=site:soundgasm.net+" + query + "&num=5"
+  response = requests.get(google_url, {"User-Agent": ua.random})
+  soup = BeautifulSoup(response.text, "html.parser")
+
+  result_div = soup.find_all('div', attrs = {'class': 'ZINbbc'})
+
+  links = []
+  titles = []
+  descriptions = []
+  for r in result_div:
+    # Checks if each element is present, else, raise exception
+    try:
+      link = r.find('a', href = True)
+      titleone = r.find('div', attrs={'class':'vvjwJb'}).get_text()
+      description = r.find('div', attrs={'class':'s3v9rd'}).get_text()
+        
+      # Check to make sure everything is present before appending
+      if link != '' and titleone != '' and description != '': 
+        links.append(link['href'])
+        titles.append(titleone)
+        descriptions.append(description)
+        
+        embed=discord.Embed(title=titles[0], url=links[0], description=descriptions[0], color=0x5b5bff)
+        embed.add_field(name="[" + titles[1] + "](" + links[1] + ")", value=description[1], inline=False)
+        embed.add_field(name="[" + titles[2] + "](" + links[2] + ")", value=description[2], inline=False)
+        embed.add_field(name="[" + titles[3] + "](" + links[3] + ")", value=description[3], inline=False)
+        await ctx.send(embed=embed)
+    # Next loop if one element is not present
+    except:
+      continue
     
 @translate.error
 async def translate_error(error, ctx):
