474822eea9ebe07ee970747c7ce2495866668f27
MyNameMadeYouSmile <58444275+MyNameMadeYouSmile@users.noreply.github.com>
2019-12-04 11:11:34 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -76,10 +76,10 @@ async def urban(ctx, *, term):
 async def hotpost(ctx, *, subreddit):
   if ";" in subreddit:
     subreddit = subreddit.replace(";", "+")
-    for submission in reddit.subreddit(subreddit).hot(limit=3):
+    for submission in reddit.subreddit(subreddit).hot(limit=3) if not submission.stickied:
       await ctx.send("""```""" + submission.title + """```""")
   else:
-    for submission in reddit.subreddit(subreddit).hot(limit=3):
+    for submission in reddit.subreddit(subreddit).hot(limit=3) if not submission.stickied:
       await ctx.send("""```""" + submission.title + """```""")
     
 @translate.error
