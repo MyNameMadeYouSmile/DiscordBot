40823851852cd00ccdda6a9292c1401cc39540aa
MyNameMadeYouSmile <58444275+MyNameMadeYouSmile@users.noreply.github.com>
2019-12-22 00:13:11 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -109,7 +109,7 @@ async def clear(ctx):
   
 @client.command(pass_context=True)
 async def searchgwa(ctx, *, searchterm):
-  if ctx.message.channel != "bot-playground":
+  if not "bot-playground" in ctx.message.channel:
     print(str(ctx.message.channel))
     await ctx.send("Go to the #bot-playground channel to use the !searchgwa command. Let's keep this channel clean.")
   else:
