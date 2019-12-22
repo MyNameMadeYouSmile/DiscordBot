39b942dacd15ee12f3a36f39d5b589d020442444
MyNameMadeYouSmile <58444275+MyNameMadeYouSmile@users.noreply.github.com>
2019-12-22 16:38:48 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -27,7 +27,8 @@ client.remove_command('help')
 @client.event
 async def on_ready():
   #activity = discord.Game(name="!help")
-  await client.change_presence(status=discord.Status.online, activity(name="Type !help"))
+  #await client.change_presence(status=discord.Status.online, activity(name="Type !help"))
+  await client.change_presence(activity.name = "Type !help")
   print("Bot Name: {}".format(client.user.name))
   print("Bot ID: {}".format(client.user.id))
   
