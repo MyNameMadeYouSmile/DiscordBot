0dd058a24fe7699c421da2b91bc654514df94c2b
MyNameMadeYouSmile <58444275+MyNameMadeYouSmile@users.noreply.github.com>
2019-12-22 19:11:10 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -40,7 +40,8 @@ async def on_command_error(ctx, error):
 @client.event
 async def on_member_join(member):
   print(">> " + member.name + " has just joined The Smiley House.")
-  await client.send(657950668853739520, "Hello " + member.mention+ "! Welcome to The Smiley House! Please go to #rules and read some of the rules you need to follow in order to stay and I hope you have fun in Mike's server :)")
+  channel = 657950668853739520
+  await channel.send("Hello " + member.mention+ "! Welcome to The Smiley House! Please go to #rules and read some of the rules you need to follow in order to stay and I hope you have fun in Mike's server :)")
   
 @client.command(pass_context=True)
 async def help(ctx):
