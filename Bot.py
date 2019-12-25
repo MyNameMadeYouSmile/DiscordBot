75550f5aa00210cd3db6f745c5b782b28ac666f5
Mike <58444275+MyNameMadeYouSmile@users.noreply.github.com>
2019-12-25 21:49:04 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -262,10 +262,6 @@ async def searchgwa_error(error, ctx):
 async def urban_error(error, ctx):
   return await error.send(error.message.author.mention + " Usage: !urban [term]")
   
-@calc.error
-async def calc_error(error, ctx):
-  return await error.send(error.message.author.mention + " Example: !calc 2 + 2 - (2 * 6)")
-  
 @_8ball.error
 async def _8ball_error(error, ctx):
   return await error.send(error.message.author.mention + " Usage: !8ball [Your question]")
