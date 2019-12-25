ea7ec605b113c5ba84dd663512d0be579e392ae8
Mike <58444275+MyNameMadeYouSmile@users.noreply.github.com>
2019-12-25 19:37:49 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -32,7 +32,7 @@ chatterbotter = False
 magicResponses = ['As I see it, yes.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.',
                  'Don’t count on it.', 'It is certain.', 'It is decidedly so.', 'Most likely.', 'My reply is no.', 'My sources say no.',
                  'Outlook not so good.', 'Outlook good.', 'Reply hazy, try again.', 'Signs point to yes.', 'Very doubtful.', 'Without a doubt.',
-                 'Yes.', 'You may rely on it.', 'Yes – definitely.']
+                 'Yes.', 'Yes – definitely.', 'You may rely on it.']
 
 client.remove_command('help')
 
@@ -109,7 +109,7 @@ async def on_message(message):
 async def chat(ctx):
   asyncio.get_event_loop().run_until_complete(chatbot(ctx))
   
-@client.command(pass_context=True)
+@client.command(pass_context=True, name="8ball")
 async def _8ball(ctx, *, question):
   await ctx.send(random.choice(magicResponses))
   
