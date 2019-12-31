f5444f89c6cada732087bede127b57154b778931
Mike <58444275+MyNameMadeYouSmile@users.noreply.github.com>
2019-12-31 17:16:50 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -246,6 +246,8 @@ async def buy(ctx, ename, imgUrl):
   dbPass = os.environ['db_password']
   dbName = 'MPbzulZgmy'
   
+  theMoney = ""
+  
   conn = pymysql.connect(host=dbServer, user=dbUser, passwd=dbPass, db=dbName)
   cur = conn.cursor()
   sql = "SELECT money FROM users WHERE username ='%s'"
@@ -262,21 +264,22 @@ async def buy(ctx, ename, imgUrl):
     await ctx.send("You don't have enough money to buy an emoji. You need 10 000 $ but you got 0 $")   
   else:
     for row in cur:
-      if int(row[0]) < 10000:
-        await ctx.send(ctx.message.author.mention + " I'm sorry, but you need 10000 $ to purchase a custom emoji. You currently have only {} $.".format(row[0]))
-      else:
-        sql3 = "INSERT INTO emojis (emoji_name, emoji_url, emoji_author) VALUES (%s, %s, %s)"
-        args2 = (ename, imgUrl, str(ctx.message.author))
-        cur.execute(sql3, args2)
-        conn.commit()
+      theMoney = row[0]
+    if int(theMoney) < 10000:
+      await ctx.send(ctx.message.author.mention + " I'm sorry, but you need 10000 $ to purchase a custom emoji")
+    else:
+      sql3 = "INSERT INTO emojis (emoji_name, emoji_url, emoji_author) VALUES (%s, %s, %s)"
+      args2 = (ename, imgUrl, str(ctx.message.author))
+      cur.execute(sql3, args2)
+      conn.commit()
         
-        sql4 = "UPDATE users SET money=%s WHERE username=%s"
-        newMoney = int(row[0]) + int("-10000")
-        args3 = (str(newMoney), str(ctx.message.author))
-        cur.execute(sql4, args3)
-        conn.commit()
+      sql4 = "UPDATE users SET money=%s WHERE username=%s"
+      newMoney = int(theMoney) + int("-10000")
+      args3 = (str(newMoney), str(ctx.message.author))
+      cur.execute(sql4, args3)
+      conn.commit()
         
-        await ctx.send(ctx.message.author.mention + " You successfully purchased new custom emoji '{}' for 10 000 $. Now you can use it :)".format(ename))
+      await ctx.send(ctx.message.author.mention + " You successfully purchased new custom emoji {} for 10 000 $. Now you can use it :)".format(ename))
         
   conn.close()
 
